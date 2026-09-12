baf726c00d79941119893eda052de77b1886f114
jguerramonterrosa-create <jguerramonterrosa@gmail.com>
2026-09-12 14:29:03 -0500

    Add files via upload

--- src/supabaseClient.js
+++ src/supabaseClient.js
@@ -1,6 +1,6 @@
 import { createClient } from '@supabase/supabase-js';
 
-const supabaseUrl = 'https://tgkfqfcjijjraqmrbxxa.supabase.co';
-const supabaseAnonKey = 'sb_publishable_CaS0boKV_p8WvPYnUSH_ng_fZ25GncB';
+const supabaseUrl = 'https://nogoiwpluafbqzwtgjny.supabase.co';
+const supabaseAnonKey = 'sb_publishable_dan4Tzb5qgckVQYYs_RgNA_8jSWmtO3';
 
 export const supabase = createClient(supabaseUrl, supabaseAnonKey);
